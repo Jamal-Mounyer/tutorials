1a818a773c16dbb4185ef37b5d97aaf791a07b8f
Jamal Aden Mounyer <jamal.aldeen.almoner1@gmail.com>
2026-07-03 21:13:01 +0300

    [IMP] estate: complete chapter 5

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,24 +1,38 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'Real Estate Property'
 
+    @staticmethod
+    def _default_date_availability(self):
+        return fields.Date.today() + relativedelta(months=3)
+
+
     name = fields.Char(required = True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy = False, default = _default_date_availability)
     expected_price = fields.Float(required = True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly = True, copy = False)
+    bedrooms = fields.Integer(default = 2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection(string='Type',
-                                          selection=[('North', 'north'),
+    garden_orientation = fields.Selection(selection=[('North', 'north'),
                                                      ('South', 'south'),
                                                      ('East', 'east'),
                                                      ('West', 'west')])
     
+    active = fields.Boolean(default = True)
+    state = fields.Selection(required = True,
+                             copy = False,
+                             default = 'New',
+                             selection = [('New', 'new'), 
+                                          ('Offer Received', 'Offer Received'),
+                                          ('Offer Accepted', 'Offer Accepted'), 
+                                          ('Sold', 'sold'),
+                                          ('Cancelled', 'Cancelled')])
