0226a47df3bf1a475e41f256626851ed7f1dd8d3
Jamal Aden Mounyer <jamal.aldeen.almoner1@gmail.com>
2026-07-04 23:52:52 +0300

    [IMP] estate: complete chapter 6

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -10,19 +10,20 @@ class EstateProperty(models.Model):
         return fields.Date.today() + relativedelta(months=3)
 
 
-    name = fields.Char(required = True)
-    description = fields.Text()
-    postcode = fields.Char()
-    date_availability = fields.Date(copy = False, default = _default_date_availability)
-    expected_price = fields.Float(required = True)
-    selling_price = fields.Float(readonly = True, copy = False)
-    bedrooms = fields.Integer(default = 2)
-    living_area = fields.Integer()
-    facades = fields.Integer()
-    garage = fields.Boolean()
-    garden = fields.Boolean()
-    garden_area = fields.Integer()
-    garden_orientation = fields.Selection(selection=[('North', 'north'),
+    name = fields.Char(required = True, string = 'Title')
+    description = fields.Text(string = 'Description')
+    postcode = fields.Char(string = 'Postcode')
+    date_availability = fields.Date(string = 'Available From', copy = False, default = _default_date_availability)
+    expected_price = fields.Float(string = 'Expected Price', required = True)
+    selling_price = fields.Float(string = 'Selling Price', readonly = True, copy = False)
+    bedrooms = fields.Integer(string = 'Bedrooms', default = 2)
+    living_area = fields.Integer(string = 'Living Area (sqm)')
+    facades = fields.Integer(string = 'Facades')
+    garage = fields.Boolean(string = 'Garage')
+    garden = fields.Boolean(string = 'Garden')
+    garden_area = fields.Integer(string = 'Garden Area (sqm)')
+    garden_orientation = fields.Selection(string = 'Garden Orientation', 
+                                          selection=[('North', 'north'),
                                                      ('South', 'south'),
                                                      ('East', 'east'),
                                                      ('West', 'west')])
